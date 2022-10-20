c4949203875d226614a8dbbc189557f18a6c0e57
banggeunho <panggeunho@gmail.com>
2022-10-20 10:50:16 +0900

    test 추가

--- src/test/java/gcukakao/jenkins/calculator/CalTest.java
+++ src/test/java/gcukakao/jenkins/calculator/CalTest.java
@@ -1,4 +1,5 @@
 package gcukakao.jenkins.calculator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -6,18 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class CalTest {
     private Calculator calculator = new Calculator();
-
+    @DisplayName("더하기")
     @Test
     public void testSum() {
         assertEquals(5, calculator.sum(2, 3));
     }
 
+    @DisplayName("빼기")
     @Test
     public void testSub() {assertEquals(1, calculator.sub(3, 2));}
 
+    @DisplayName("곱하기")
     @Test
     public void testMul() { assertEquals(10, calculator.mul(2, 5));}
 
+    @DisplayName("나누기")
     @Test
     public void testDiv() { assertEquals(6, calculator.div(12,2));}
 }
